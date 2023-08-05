e473601b2323bff1c2a2694c43d7238cd71c44ba
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2023-08-05 14:35:37 -0500

    the pie chart was added

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,19 +1,46 @@
 import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function BudgetControl({ budget, expenses }) {
 
     const [available, setAvailable] = useState(0);
     const [spent, setSpent] = useState(0);
+    const [percentage, setPersentage] = useState(100);
+    const [pathColor, setPathColor] = useState("");
+    const [textColor, setTextColor] = useState("");
 
     useEffect(() => {
       const totalSpent = expenses.reduce( (total, expense) => {
         return total + expense.quantity
       }, 0)
 
-      const totalAvailable = budget - totalSpent
+      const totalAvailable = budget - totalSpent;
+
+      // calcular el porcentaje de la grafica
+      const newPercentage = ((totalAvailable * 100 / budget)).toFixed(2);
 
       setSpent(totalSpent);
       setAvailable(totalAvailable); 
+
+      setTimeout(()=> {
+        setPersentage(newPercentage);
+
+        if (newPercentage >= 66.66){
+            setPathColor("#64CC45");
+            setTextColor("#64CC45");
+          }
+        else if(newPercentage >= 33.33) {
+            setPathColor("#E2E057");
+            setTextColor("#E2E057");
+        }
+        else {
+            setPathColor("#EB4848");
+            setTextColor("#EB4848");
+        }
+      }, 700)
+
+
     }, [expenses])
     
 
@@ -28,7 +55,15 @@ function BudgetControl({ budget, expenses }) {
     return (
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>
-                <p>Grafica aqui</p>
+                <CircularProgressbar
+                styles={buildStyles({
+                    pathColor: pathColor,
+                    textColor: textColor,
+                    trailColor: "#f5f5f5"
+                })}
+                value={percentage}
+                text={`${percentage}% Available`}
+                />
             </div>
 
             <div className="contenido-presupuesto">
